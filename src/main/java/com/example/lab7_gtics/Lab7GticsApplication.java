16e8fc504b64c9d52c9fbc8f95a83351aa7941f6
unknown <luis.ramost@pucp.edu.pe>
2023-05-30 22:21:13 -0500

    dev

---
+++ src/main/java/com/example/lab7_gtics/Lab7GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7GticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab7_gtics/Lab7GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7GticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
